3656fef261ae15d1da3fb5a04471a4a91d4a903c
sensei07 <atomkiv17@gmail.com>
2023-01-02 15:47:13 +0200

    created the initial structure, installed MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+import { ROUTES, ADMIN_ROUTES } from './constants/routes';
+import { NotFound } from './pages/NotFound';
+import { Header } from './components/Header';
+
+const role = 'ADMIN';
+
+const App: FC = () => {
+  const routes = role === 'ADMIN' ? [...ADMIN_ROUTES, ...ROUTES] : ROUTES;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.element />}
+          />
+        ))}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
